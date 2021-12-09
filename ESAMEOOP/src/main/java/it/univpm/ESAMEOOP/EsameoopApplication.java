c89e1163940ce6cae9cc749a51f1e3ff1d886aa7
NicoloIanni <93644541+NicoloIanni@users.noreply.github.com>
2021-12-09 18:10:49 +0100

    Add files via upload

---
+++ ESAMEOOP/src/main/java/it/univpm/ESAMEOOP/EsameoopApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.ESAMEOOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsameoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsameoopApplication.class, args);
+	}
+
+}
