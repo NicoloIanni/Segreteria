79d57428b53ecab03ce3f96fde518ad34575b02f
NicoloIanni <93644541+NicoloIanni@users.noreply.github.com>
2021-12-09 18:15:32 +0100

    Add files via upload

---
+++ ESAMEOOP/src/test/java/it/univpm/ESAMEOOP/EsameoopApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.ESAMEOOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsameoopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
